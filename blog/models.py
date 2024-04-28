8e82f910463fc52a6bd1e2ea1548d598441bbeae
Bazhen <149209652+BazhenMokhovtsov@users.noreply.github.com>
2024-04-28 17:51:33 +0200

    add new model UserProfile

--- blog/models.py
+++ blog/models.py
@@ -26,6 +26,7 @@ class Posts(models.Model):
     update_date = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
     published = models.BooleanField(verbose_name="Публикация", default=False)
     category = models.ForeignKey(Category, verbose_name="Категории", on_delete=models.CASCADE)
+    # Аплоуд ту пока не ясен для поля Изображения
     image = models.ImageField(verbose_name="Изображения", upload_to=None, height_field=300, width_field=300, max_length=None)
     slug = models.SlugField(verbose_name='Слаг')
 
@@ -47,3 +48,26 @@ class Comments(models.Model):
     post = models.ForeignKey(Posts, verbose_name="Пост/Новость", on_delete=models.CASCADE)
     text = models.TextField(verbose_name="Текст")
     created_date = models.DateTimeField(verbose_name="Дата добавления коментария", auto_now_add=True)
+
+    def __str__(self):
+        return self.author
+    
+    class Meta:
+         verbose_name = "Коментарий"
+         verbose_name_plural = "Коментарии"
+
+    
+class UserProfile(models.Model):
+    last_name = models.CharField(verbose_name="Имя", max_length=50)
+    first_name =models.CharField(verbose_name="Фамилия", max_length=50)
+    birth_date = models.DateField(verbose_name="Дата рождения", null=True, blank=True)
+    city = models.CharField(verbose_name="Город", max_length=50)
+    e_mail = models.CharField(verbose_name="Электронная почта", max_length=255)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        verbose_name = "Профайл пользователя"
+        verbose_name_plural = "Профайл пользователей"
+    
